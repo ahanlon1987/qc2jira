0901a4fabd3ffc20c3f6fc6c3250617c6bb4933f
ahanlon1987 <ahanlon@gmail.com>
2014-03-31 18:21:11 -0500

    Writing output to file

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 ##QC to JIRA
 This is a lightweight node app that will read in requirement #'s from a text file, execute a RESTful lookup for those issues, and then return the corresponding JIRA #'s if applicable.
 
-
+Steps to run:
+# Install node on your computer http://nodejs.org/
+# Install Node Package Manager - curl http://npmjs.org/install.sh | sh
+# Clone this repo to your local machine
+# Take the weekly requirement extract, transform the "requirement name" column into requirement number by:
+#* In a new column, run =LEFT(G2,FIND(" ", G2)) WHERE G2 is the column of the "Requirement Name"
+#* Cut and paste the result of that command into "test.xls" in the qc2jira directory
+# From that directory, run npm install then run node app.js
+# Open the output.txt file, sort by the first column A-Z
+# Paste that into the requirements extract, viola, 1-1 mapping of requirements to JIRA issues
 
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var JiraApi = require("jira").JiraApi;
 var excel = require("excel");
 var Backbone = require("backbone");
+var nodeFS = require("fs");
 
 var config = {
     "user": "jirabot",
@@ -11,8 +12,10 @@ var config = {
 
 
 var SPRINT_FIELD = 'customfield_10550';
-
 var finalMapping = new Backbone.Collection();
+var numberOfRequirements = 0;
+var outputString = '';
+
 
 excel('test.xlsx', function(error, data){
     console.log('inside parse excel');
@@ -23,24 +26,24 @@ excel('test.xlsx', function(error, data){
     }
 
     var requirementNumberCollection = new Backbone.Collection(data);
-    console.log("Number of requirements found: "  + requirementNumberCollection.size());
 
+    numberOfRequirements = requirementNumberCollection.size();
+    console.log("Number of requirements found: "  + numberOfRequirements);
 
     requirementNumberCollection.forEach(function(model){
-
         if(model.get(0)){
             console.log("Looking for attribute: " + model.get(0));
             var issueNumber = lookupIssue(model.get(0));
         }
-
     });
-
 });
 
 
 
 
 function lookupIssue(requirementNumber) {
+
+
     var query = 'text~' + '"' + requirementNumber + '"';
     console.log("inside of lookupIssue(), looking for: " + query);
     var jira = new JiraApi('https', config.host, config.port, config.user, config.password, '2');
@@ -68,12 +71,13 @@ function lookupIssue(requirementNumber) {
                 sprint = fields[SPRINT_FIELD];
                 if(sprint != null){
                     var charNum = sprint[0].search("name=") + 5;
-                    sprints += sprint[0].substr(charNum, 9);
+                    sprints += delim + sprint[0].substr(charNum, 9);
                 }
                 delim = ', ';
 
             });
         }
+
 
         finalMapping.add({
             requirementNumber: requirementNumber,
@@ -81,7 +85,19 @@ function lookupIssue(requirementNumber) {
             sprint:sprints
         });
 
-        console.log(JSON.stringify(finalMapping));
+        outputString += requirementNumber + ' | ' + (keys ? keys : 'NO JIRA FOUND') + ' | ' + ( sprints ? sprints : 'NO SPRINT FOUND') + '\n';
+
+        if (finalMapping.size() == numberOfRequirements) {
+            nodeFS.writeFile("output.txt", outputString, function(error) {
+
+                if (error){
+                   console.log(error);
+                }
+
+                console.log("Finished writing to output.txt.");
+            })
+        }
+
     });
 }
 
